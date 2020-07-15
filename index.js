bfc7ffb3deff659fc81bc63f90c1d64c5dca4182
carcarvroom <carly_l@outlook.com>
2020-07-14 22:34:45 -0700

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+  newTime = Number.parseInt(time)
+  if (isNaN(newTime)) {
+    return "Incorrect format"
+  } else if (newTime < 12) {
+    return "Good Morning"
+  } else if (newTime >= 12 && newTime <= 17) {
+    return "Good Afternoon"
+  } else {  
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message){
+  let greeting = document.getElementById('greeting')
+  greeting.innerText = message
+}
